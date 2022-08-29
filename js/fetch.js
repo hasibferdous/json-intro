b6f09958b611a660b36e9757fd3deb761173c736
hasibferdous <hasibferdous.ict@gmail.com>
2022-08-29 11:38:36 +0600

    json

---
+++ js/fetch.js
@@ -0,0 +1,5 @@
+console.log("fetching");
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
---
+++ js/json.js
@@ -0,0 +1,5 @@
+const user = {id:1, name: 'amir', job: 'actor'};
+//javascript object notation(json)
+const stringified = JSON.stringify(user);
+console.log(user);
+console.log(stringified);
