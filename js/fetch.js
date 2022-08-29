0cab3dabaf42c325bd8991a95a02cb973cbc9c28
hasibferdous <hasibferdous.ict@gmail.com>
2022-08-29 11:45:21 +0600

    fetching url

--- js/fetch.js
+++ js/fetch.js
@@ -3,3 +3,10 @@ console.log("fetching");
 fetch('https://jsonplaceholder.typicode.com/todos/1')
   .then(response => response.json())
   .then(json => console.log(json))
+
+
+function loadData() {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
+}
