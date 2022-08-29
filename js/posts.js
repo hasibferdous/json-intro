2a0e9c4b85e927f7e7b61cdaa48f68874c87e2ff
hasibferdous <hasibferdous.ict@gmail.com>
2022-08-29 13:46:16 +0600

    multiple posting with css

--- js/posts.js
+++ js/posts.js
@@ -1,18 +1,28 @@
-function loadPosts(){
-    console.log('loading Post data');
+function loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => displayPosts(data))
+}
+//steps:
+//1.get the container element where you want to add the new elements
+//2.Create Child element
+//3.set innerText or innerHTML
+//4.appendChild
+
+function displayPosts(posts){
+    
+    const postsContainer = document.getElementById('post-container');
+    for(const post of posts){ 
+        const postDiv = document.createElement('div');
+        postDiv.classList.add('post')
+        console.log(post);
+        postDiv.innerHTML = `
+        <h4>User-${post.userId}</h5>
+        <h5>Title: ${post.title}</h6>
+        <p>Post Description:${post.body}</p>
+        `;
+        postsContainer.appendChild(postDiv);
+        }
 }
 
 loadPosts();
-
-function loadPosts2() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(data => displayPost2(data))
-}
-
-function displayPost2(posts){
-    for(const post of posts){ 
-    console.log(post);
-    }
-}
-loadPosts2();
