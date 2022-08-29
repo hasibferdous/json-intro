3ba4df2238e3f8914996abe1fc590efcf57cd9ca
hasibferdous <hasibferdous.ict@gmail.com>
2022-08-30 01:15:18 +0600

    countries

--- js/json.js
+++ js/json.js
@@ -1,5 +1,6 @@
 const user = {id:1, name: 'amir', job: 'actor'};
 //javascript object notation(json)
+//JSON.stringify
 const stringified = JSON.stringify(user);
 console.log(user);
 console.log(stringified);
