f5705d7270ba8f6ae6ae5818d6061662d76bc01c
hasibferdous <hasibferdous.ict@gmail.com>
2022-08-30 23:50:02 +0600

    dynamic url

--- js/countries.js
+++ js/countries.js
@@ -16,8 +16,22 @@ const displayCountries = countries =>{
         countryDiv.innerHTML = `
         <h3>Name:${country.name.common} </h3>
         <p>Capital: ${country.capital ? country.capital[0]: 'No Capital'}</p>
+        <button onclick="loadCountryDetail('${country.cca2}')">Details</button>
         `;
         countriesContainer.appendChild(countryDiv);
     })
 }
+const loadCountryDetail = (code) => {
+    const url =`https://restcountries.com/v3.1/alpha/${code}`
+    //console.log('Get country details',code);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountryDetail(data[0]))
+}
+const displayCountryDetail = country =>{
+    const CountryDetail = document.getElementById('country-detail');
+    CountryDetail.innerHTML = `<h2>Details: ${country.name.common}</h2>
+    <img src="${country.flags.png}">    `
+}
+
 loadCountries();
