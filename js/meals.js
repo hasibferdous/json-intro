8040ab7d9b1b92b41bdf4f60524ee20eae4b1be3
hasibferdous <hasibferdous.ict@gmail.com>
2022-09-01 00:46:28 +0600

    meal db details

--- js/meals.js
+++ js/meals.js
@@ -13,7 +13,7 @@ const displayMeals = meals =>{
         const mealDiv = document.createElement('div'); 
         mealDiv.classList.add('col');
         mealDiv.innerHTML = `
-        <div class="card h-100">
+        <div onclick="loadMealDetail(${meal.idMeal})" class="card h-100">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
             <h5 class="card-title">${meal.strMeal}</h5>
@@ -32,4 +32,29 @@ const searchFood = () => {
     loadMeals(searchText);
     searchField.value = '';
 }
-//loadMeals();
+const loadMealDetail = (idMeal) =>{
+    //console.log(idMeal)
+    const url =  `https://www.themealdb.com/api/json/v1/1/search.php?i=${idMeal}`
+    //console.log(url);
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMealDetails(data.meals));
+}
+
+const displayMealDetails = meal => {
+    const detailContainer = document.getElementById('detail-container')
+    const mealDiv = document.createElement('div');
+    mealDiv.classList.add('card');
+    mealDiv.innerHTML = `
+    //<img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">${meal.strMeal}</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    `;
+    detailContainer.appendChild(mealDiv);
+}
+
+loadMeals('');
